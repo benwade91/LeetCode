c470d0844f107809c571cda10bb11f7141b644d3
Ben Wade <70988204+benwade91@users.noreply.github.com>
2022-08-02 13:51:11 -0700

    Time: 102 ms (63.03%), Space: 53.7 MB (5.24%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.js
+++ 125-valid-palindrome/125-valid-palindrome.js
@@ -4,6 +4,5 @@
  */
 var isPalindrome = function(s) {
     let clean = s.toLowerCase().split('').filter(a=> a.match(/[a-z0-9]/))
-    console.log(s.toLowerCase())
     return clean.join('') === clean.reverse().join('')
 };
