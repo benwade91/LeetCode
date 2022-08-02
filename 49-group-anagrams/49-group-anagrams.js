23d78fe9682057b3828f91f349c8b0badd8b6514
Ben Wade <70988204+benwade91@users.noreply.github.com>
2022-08-02 10:25:36 -0700

    Time: 201 ms (55.37%), Space: 57.5 MB (7.66%) - LeetHub

---
+++ 49-group-anagrams/49-group-anagrams.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+    let res = [];
+    let map = {};
+    for(const word of strs){
+        const sortedWord = word.split('').sort().join('');
+        map[sortedWord] = [...(map[sortedWord] ?? []), word]
+    }
+    for(const word in map) res.push([...map[word]])
+    console.log(map)
+    return res
+};
