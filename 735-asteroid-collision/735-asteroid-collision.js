ca86c1885adf17aa89e538083b4258bb0229694f
Ben Wade <70988204+benwade91@users.noreply.github.com>
2022-08-02 15:21:04 -0700

    Time: 1462 ms (5.00%), Space: 52.6 MB (5.40%) - LeetHub

---
+++ 735-asteroid-collision/735-asteroid-collision.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[]} asteroids
+ * @return {number[]}
+ */
+var asteroidCollision = function(asteroids) {
+    const res = [];
+    let max = asteroids[0]
+    for(let i = 0; i < asteroids.length; i++){
+        let curr = asteroids[i]
+        if(curr > 0) res.push(curr);
+        else if(curr < 0 && !res.length) res.push(curr);
+        else{
+            console.log(res)
+            while(res.length){
+                if(res[res.length-1] < 0) {
+                    res.push(curr);
+                    break;
+                }
+                if(res[res.length-1] + curr > 0) break;
+                if(res[res.length-1] + curr == 0){
+                    res.pop();
+                    break;
+                }
+                if(res[res.length-1] + curr < 0){
+                    res.pop();
+                }
+                if(!res.length){
+                    res.push(curr);
+                    break;
+                }
+            }
+        }
+    }
+    return res
+};
