a7e8be4dc198d0e54adb31a549b0725a8736b5d1
Ben Wade <70988204+benwade91@users.noreply.github.com>
2022-08-04 14:34:37 -0700

    Time: 115 ms (24.45%), Space: 44.1 MB (33.89%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.js
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.js
@@ -3,12 +3,12 @@
  * @return {number}
  */
 var minCostClimbingStairs = function(cost) {
-    let curr = cost[1];
-    let prev = cost[0];
-    for(let i = 2; i < cost.length; i++){
-        let temp = Math.min(curr, prev) + cost[i]
-        prev = curr
-        curr = temp
+    let dp = [];
+    let n = cost.length-1;
+    dp[0] = cost[0]
+    dp[1] = cost[1]
+    for(let i = 2; i<=n; i++){
+        dp[i] = cost[i] + Math.min(dp[i-1], dp[i-2])
     }
-    return Math.min(prev, curr);
+    return Math.min(dp[n], dp[n-1]);
 };
