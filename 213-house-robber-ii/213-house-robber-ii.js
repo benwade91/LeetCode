ef8a1310829a1e9e755497df7e98e56db7b86440
Ben Wade <70988204+benwade91@users.noreply.github.com>
2022-08-04 15:21:47 -0700

    Time: 111 ms (15.51%), Space: 44.9 MB (5.63%) - LeetHub

---
+++ 213-house-robber-ii/213-house-robber-ii.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function(nums) {
+    if(nums.length < 4) return Math.max(...nums)
+    let dp1 = [nums[0]]
+    let dp2 = [0, nums[1]]
+    for(let i = 1; i < nums.length-1; i++){
+        dp1[i] = Math.max(dp1[i-1], (dp1[i-2] || 0) + nums[i])
+    }
+    for(let i = 2; i < nums.length; i++){
+        dp2[i] = Math.max(dp2[i-1], dp2[i-2] + nums[i])
+    }
+    console.log(dp1,dp2)
+    return Math.max(dp1.pop(), dp2.pop())
+};
