c544f137725d9a281230d4ee89804dec668aedef
Ben Wade <70988204+benwade91@users.noreply.github.com>
2022-08-04 10:20:45 -0700

    Time: 88 ms (46.60%), Space: 41.7 MB (69.91%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    if(n < 3) return n;
+    let a = 1;
+    let b = 2;
+    while(n-- > 2){
+        let temp = b;
+        b += a;
+        a = temp;
+    }
+    return b
+};
