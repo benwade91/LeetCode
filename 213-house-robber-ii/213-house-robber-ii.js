7907b740cb0b5e17ffb223ea8bae4d1bc9f0da35
Ben Wade <70988204+benwade91@users.noreply.github.com>
2022-08-06 10:13:26 -0700

    Time: 72 ms (79.36%), Space: 41.8 MB (72.37%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.js
+++ 213-house-robber-ii/213-house-robber-ii.js
@@ -3,14 +3,13 @@
  * @return {number}
  */
 var rob = function(nums) {
-    if(nums.length < 4) return Math.max(...nums)
-    let dp1 = [nums[0]]
-    let dp2 = [0, nums[1]]
+    let dp1 = [nums[0]];
+    let dp2 = [0, (nums[1] || 0)];
     for(let i = 1; i < nums.length-1; i++){
-        dp1[i] = Math.max(dp1[i-1], (dp1[i-2] || 0) + nums[i])
+        dp1[i] = Math.max( nums[i] + (dp1[i-2] || 0), dp1[i-1])
     }
     for(let i = 2; i < nums.length; i++){
-        dp2[i] = Math.max(dp2[i-1], dp2[i-2] + nums[i])
+        dp2[i] = Math.max( nums[i] + (dp2[i-2] || 0), dp2[i-1])
     }
-    return Math.max(dp1.pop(), dp2.pop())
+    return Math.max(dp1[dp1.length-1], dp2[dp2.length-1])
 };
