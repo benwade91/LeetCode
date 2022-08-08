aafc092c89406d67ae4e54b62aeaceb1c57062f5
Ben Wade <70988204+benwade91@users.noreply.github.com>
2022-08-08 14:42:56 -0700

    Time: 109 ms (16.79%), Space: 42 MB (88.20%) - LeetHub

--- 226-invert-binary-tree/226-invert-binary-tree.js
+++ 226-invert-binary-tree/226-invert-binary-tree.js
@@ -12,9 +12,7 @@
  */
 var invertTree = function(root) {
     if(!root) return root;
-    let temp = root.left;
-    root.left = root.right;
-    root.right = temp;
+    [root.left, root.right] = [root.right, root.left]
     invertTree(root.left)
     invertTree(root.right)
     return root
