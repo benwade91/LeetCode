715568537c31571c8f3162d76561f26b3157f7c6
Ben Wade <70988204+benwade91@users.noreply.github.com>
2022-08-09 10:01:36 -0700

    Time: 107 ms (96.29%), Space: 47.8 MB (48.01%) - LeetHub

--- 322-coin-change/322-coin-change.js
+++ 322-coin-change/322-coin-change.js
@@ -3,13 +3,13 @@
  * @param {number} amount
  * @return {number}
  */
-var coinChange = function(coins, amount){
-    let dp = Array(amount + 1).fill(Infinity);
-    dp[0] = 0;
+var coinChange = function(coins, amount) {
+    let wallet = Array(amount+1).fill(Infinity);
+    wallet[0] = 0;
     for(let coin of coins){
         for(let i = coin; i <= amount; i++){
-            dp[i] = Math.min(dp[i], dp[i - coin] + 1)
+            wallet[i] = Math.min(wallet[i], wallet[i-coin]+1);
         }
     }
-    return dp[amount] === Infinity ? -1 : dp[amount]
+    return wallet[amount] === Infinity ? -1 : wallet[amount]
 };
