8a636f3d48b2bed30a952fa23781ca8e71678034
Ben Wade <70988204+benwade91@users.noreply.github.com>
2022-08-17 08:23:57 -0700

    Time: 75 ms (99.35%), Space: 54.4 MB (6.66%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.js
+++ 53-maximum-subarray/53-maximum-subarray.js
@@ -4,8 +4,10 @@
  */
 var maxSubArray = function(nums) {
     let dp = [nums[0]];
+    let max = nums[0];
     for(let i = 1; i<nums.length; i++){
         dp[i] = Math.max(dp[i-1] + nums[i], nums[i])
+        max = Math.max(max, dp[i])
     }
-    return Math.max(...dp)
+    return max
 };
