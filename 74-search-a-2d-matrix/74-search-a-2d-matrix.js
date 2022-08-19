0a56e927d106645dadcd51211be8f7dfa0fc39b9
Ben Wade <70988204+benwade91@users.noreply.github.com>
2022-08-19 09:32:01 -0700

    Time: 97 ms (37.80%), Space: 42.2 MB (58.14%) - LeetHub

---
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function(matrix, target) {
+    for(let i = 0; i < matrix.length; i++){
+        if(matrix[i][matrix[0].length-1] == target) return true
+        if(matrix[i][matrix[0].length-1] < target) continue;
+        if(matrix[i][0] > target) return false;
+        let l = 0, r = matrix[0].length-1;
+        while(l < r){
+            let mid = Math.floor((l+r)/2);
+            if(matrix[i][mid] == target || matrix[i][l] == target) return true;
+            if(matrix[i][mid] < target) l = mid+1;
+            else r = mid-1
+        }
+        
+        return matrix[i][l] === target
+    }
+    return false
+};
