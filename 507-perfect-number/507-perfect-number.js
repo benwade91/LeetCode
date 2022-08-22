cb07634ab946536c40a4179ab9460f14ea0c7a5d
Ben Wade <70988204+benwade91@users.noreply.github.com>
2022-08-22 12:33:19 -0700

    Time: 2023 ms (70.57%), Space: 42.2 MB (32.29%) - LeetHub

--- 507-perfect-number/507-perfect-number.js
+++ 507-perfect-number/507-perfect-number.js
@@ -3,9 +3,11 @@
  * @return {boolean}
  */
 var checkPerfectNumber = function(num) {
+    if(num <= 1) return false
     let res = 0;
-    for(let i = 1; i < num; i++){
+    for(let i = 1; i < num/2+1; i++){
         if(num % i == 0) res += i;
     }
+    console.log(res)
     return res == num
 };
