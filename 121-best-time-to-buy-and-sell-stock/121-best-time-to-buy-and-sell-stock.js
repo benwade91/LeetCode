0f2966a5997a0075e51cfe7e0143346876cda465
Ben Wade <70988204+benwade91@users.noreply.github.com>
2022-08-23 07:54:32 -0700

    Time: 84 ms (96.91%), Space: 51.8 MB (39.40%) - LeetHub

---
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let res = 0;
+    let l = 0
+    let r = 1
+    while( l < r && r < prices.length){
+        if(prices[l] < prices[r]){
+            res = Math.max(prices[r]-prices[l], res);
+        } else l = r;
+        r++;
+    }
+    return res
+};
