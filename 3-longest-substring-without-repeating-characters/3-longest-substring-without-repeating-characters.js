f93e1286fddfcac280ba372717fa86474ea9c187
Ben Wade <70988204+benwade91@users.noreply.github.com>
2022-08-24 11:10:54 -0700

    Time: 252 ms (29.31%), Space: 49.3 MB (23.65%) - LeetHub

---
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    let res = 0, l = 0, r = 0, map = {}
+    while(r < s.length){
+        if(map[s[r]]){
+            while(map[s[r]]){
+                map[s[l]] = 0;
+                l++
+            }
+        } else {
+            map[s[r]] = 1;
+            res = Math.max(res, 1+r-l);
+            r++
+        }
+    }
+    return res
+};
