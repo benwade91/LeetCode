6d20d754b2bf6b393cd1eb08039faff4bef8fdca
Ben Wade <70988204+benwade91@users.noreply.github.com>
2022-08-25 11:45:10 -0700

    Time: 193 ms (61.33%), Space: 62.1 MB (30.07%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.js
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.js
@@ -8,14 +8,19 @@ var longestConsecutive = function(nums) {
     for(let num of set){
         let count = 0;
         let temp = num-1
+//         count backwards from the number, deleting the numbers from the set as you go
         while(set.has(temp)){
             count++
             set.delete(temp)
             temp--
         }
-        while(set.has(num++)){
+//         count from the number going forward, deleting as you go
+        while(set.has(num)){
             count++
-        }
+            set.delete(num)
+            num++
+        } 
+        
         max = Math.max(max, count)
     }
     return max
