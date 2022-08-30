0df096c64ebadffe44e79086295c926b4788d885
Ben Wade <70988204+benwade91@users.noreply.github.com>
2022-08-30 08:20:54 -0700

    Time: 119 ms (92.23%), Space: 45.9 MB (80.76%) - LeetHub

--- 322-coin-change/322-coin-change.js
+++ 322-coin-change/322-coin-change.js
@@ -4,12 +4,12 @@
  * @return {number}
  */
 var coinChange = function(coins, amount) {
-    let wallet = Array(amount+1).fill(Infinity);
+    let wallet = new Array(amount+1).fill(Infinity);
     wallet[0] = 0;
     for(let coin of coins){
         for(let i = coin; i <= amount; i++){
-            wallet[i] = Math.min(wallet[i], wallet[i-coin]+1);
+            wallet[i] = Math.min(wallet[i], wallet[i-coin]+1)
         }
     }
-    return wallet[amount] === Infinity ? -1 : wallet[amount]
+    return wallet[amount] == Infinity ? -1 : wallet[amount];
 };
