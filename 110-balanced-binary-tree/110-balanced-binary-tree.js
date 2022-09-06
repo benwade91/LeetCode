ce7fb7f261503206f894e798834e05ae5fdbd052
Ben Wade <70988204+benwade91@users.noreply.github.com>
2022-09-05 19:24:18 -0700

    Time: 153 ms (9.43%), Space: 47.4 MB (45.25%) - LeetHub

---
+++ 110-balanced-binary-tree/110-balanced-binary-tree.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isBalanced = function(root) {
+    let balanced = true
+    const helper = (node, depth=0) => {
+        if(!node) return -1;
+        let left = helper(node.left, depth+1)+1;
+        let right = helper(node.right, depth+1)+1;
+        if (Math.abs(left - right) > 1) balanced = false
+        return Math.max(left, right);
+    }
+    
+    helper(root)
+    return balanced
+};
