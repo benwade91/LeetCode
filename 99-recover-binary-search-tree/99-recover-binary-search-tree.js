6382e7a1f29bf033995a42e499c05fd97b2adcd6
Ben Wade <70988204+benwade91@users.noreply.github.com>
2022-09-12 09:57:20 -0700

    Time: 241 ms (31.73%), Space: 52.5 MB (30.88%) - LeetHub

--- 99-recover-binary-search-tree/99-recover-binary-search-tree.js
+++ 99-recover-binary-search-tree/99-recover-binary-search-tree.js
@@ -11,35 +11,23 @@
  * @return {void} Do not return anything, modify root in-place instead.
  */
 var recoverTree = function(root) {
-    const inorder = (node, nums=[]) => {
-        if(!node) return;
-        inorder(node.left, nums)
-        nums.push(node.val);
-        inorder(node.right, nums);
-    }
-    
-    const findSwapped = (nums, x=0, y=0) => {
-        for(let i = 0; i < nums.length; i++){
-            if(nums[i+1] < nums[i]){
-                y = nums[i+1];
-                if(!x) x = nums[i];
-                else break;
-            }
+    let stack = [];
+    let x=null,y=null,prev=null;
+    while(root || stack.length){
+        while(root){
+            stack.push(root);
+            root = root.left;
         }
-        return [x,y]
+        root = stack.pop();
+        if(prev && root.val < prev.val){
+            y = root;
+            if(!x) x = prev;
+            else break;
+        }
+        prev = root;
+        root = root.right;
     }
-    
-    const swapTree = (node, x, y) =>{
-        if(!node) return;
-        if(node.val === x) node.val = y;
-        else if (node.val === y) node.val = x;
-        swapTree(node.left, x, y)
-        swapTree(node.right, x, y)
-    }
-    
-    let list = new Array();
-    inorder(root, list);
-    const [x, y] = findSwapped(list)
-    swapTree(root, x, y);
-    return root
+    let temp = x.val;
+    x.val = y.val;
+    y.val = temp;
 };
