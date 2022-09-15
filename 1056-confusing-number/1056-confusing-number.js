01fab18b4d74e9d656d46c3c1664cbd7c895f337
Ben Wade <70988204+benwade91@users.noreply.github.com>
2022-09-15 09:29:48 -0700

    Time: 116 ms (5.55%), Space: 41.8 MB (86.11%) - LeetHub

--- 1056-confusing-number/1056-confusing-number.js
+++ 1056-confusing-number/1056-confusing-number.js
@@ -3,13 +3,12 @@
  * @return {boolean}
  */
 var confusingNumber = function(num) {
-    const nums = new Set([0,1,6,8,9])
-    const numsMap = {0:0, 1:1, 6:9, 8:8, 9:6};
+    const numsMap = new Map([[0,0], [1,1], [6,9], [8,8], [9,6]]);
     let rotated = 0
     let n = num
     while(n > 0){
-        if(!nums.has(n%10)) return false;
-        rotated = (rotated * 10) + numsMap[n%10]
+        if(!numsMap.has(n%10)) return false;
+        rotated = (rotated * 10) + numsMap.get(n%10)
         n = Math.floor(n/10);
     }
     return num !== rotated
