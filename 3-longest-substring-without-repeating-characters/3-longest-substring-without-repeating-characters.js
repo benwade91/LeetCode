54ca728ecb59bb0c51f93de8ee39054a186e107f
Ben Wade <70988204+benwade91@users.noreply.github.com>
2022-09-22 09:16:20 -0700

    Time: 205 ms (33.15%), Space: 49 MB (27.95%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
@@ -7,15 +7,19 @@ var lengthOfLongestSubstring = function(s) {
     let max = 0;
     let l = 0;
     for(let r = 0; r < s.length; r++){
+//         check if character is in map. if not assign val to 1
         if(!map[s[r]]){
             map[s[r]] = 1
         } else {
+//             while char is in map, delete chars from map starting from left
             while(map[s[r]]){
                 map[s[l]] = 0
                 l++
             }
         }
+//         assign char value of right index to 1
         map[s[r]] = 1
+//         update max
         max = Math.max(max, r-l+1)
     }
     return max
