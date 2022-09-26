31bba493137ceacf2b13588470caa43a39b780ab
Ben Wade <70988204+benwade91@users.noreply.github.com>
2022-09-26 11:03:35 -0700

    Time: 141 ms (6.96%), Space: 44.7 MB (15.02%) - LeetHub

--- 946-validate-stack-sequences/946-validate-stack-sequences.js
+++ 946-validate-stack-sequences/946-validate-stack-sequences.js
@@ -5,14 +5,13 @@
  */
 var validateStackSequences = function(pushed, popped) {
     let stack = [];
-    let size = pushed.length;
     let i = 0;
     for(let num of pushed){
         stack.push(num);
-        while(stack && i < size && stack[stack.length -1] === popped[i]){
+        while(stack && i < pushed.length && stack[stack.length -1] === popped[i]){
             stack.pop()
             i++
         }
     }
-    return i === size
+    return i === pushed.length
 };
