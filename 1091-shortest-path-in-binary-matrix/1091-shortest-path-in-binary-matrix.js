fde9b0b3950fdce8c9513a6779c638e9250a4d6a
Ben Wade <70988204+benwade91@users.noreply.github.com>
2022-09-29 10:42:19 -0700

    Time: 143 ms (82.73%), Space: 49.9 MB (56.50%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.js
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.js
@@ -4,15 +4,20 @@
  */
 var shortestPathBinaryMatrix = function(m) {
     const directions = [[1,1], [0,1], [1,0], [0,-1], [-1,-1], [-1,0], [-1,1], [1,-1]];
-    
+    if(m[0][0] == 1) return -1
     let queue = [[0,0,1]]
     while(queue.length){
         let [r, c, count] = queue.shift();
-        if(r < 0 || r > m.length-1 || c > m[0].length-1 || c < 0 || m[r][c] == 1) continue;
-        if(r==m.length-1 && c == m[0].length-1 && m[r][c] == 0) return count;
+        if(r==m.length-1 && c == m[0].length-1) return count;
         m[r][c] = 1
         for(let [row, col] of directions){
-            queue.push([r+row, c+col, count+1])
+            let x = r+row
+            let y = c+col
+            if(x < 0 || x > m.length-1 || 
+               y < 0 || y > m[0].length-1 || 
+               m[x][y] == 1) continue;
+            else queue.push([x,y,count+1])
+            m[x][y] = 1
         }
     }
     return -1
