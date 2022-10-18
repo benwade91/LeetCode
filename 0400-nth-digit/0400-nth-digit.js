e4c8b5bd8a0f9edc76e7eb02c1d519b572f4e767
Ben Wade <70988204+benwade91@users.noreply.github.com>
2022-10-18 10:12:08 -0700

    Time: 101 ms (31.43%), Space: 42.2 MB (40.00%) - LeetHub

---
+++ 0400-nth-digit/0400-nth-digit.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var findNthDigit = function(n) {
+    let first = 1;
+    let length = 1;
+    let digits = 9;
+    while(n > length * digits){
+        n -= length*digits;
+        first *= 10;
+        digits *= 10;
+        length += 1;
+    }
+    first += Math.floor((n-1)/length)
+    first = String(first);
+    console.log(first, n% length)
+    return Number(first[(n-1) % length])
+};
