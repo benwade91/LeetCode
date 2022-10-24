956d7ff58ea0811411ea698e84c0597257524756
Ben Wade <70988204+benwade91@users.noreply.github.com>
2022-10-24 10:42:44 -0700

    Time: 89 ms (70.84%), Space: 42.1 MB (88.26%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
@@ -3,10 +3,8 @@
  * @return {number[]}
  */
 var runningSum = function(nums) {
-    let num = 0;
-    for(let i = 0; i < nums.length; i++){
-        nums[i] += num;
-        num = nums[i]
+    for(let i = 1; i < nums.length; i++){
+        nums[i] += nums[i-1];
     }
     return nums;
 };
