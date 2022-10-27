a1554dadb48f8048bc9b846d962432e72bf590d8
Ben Wade <70988204+benwade91@users.noreply.github.com>
2022-10-27 08:29:22 -0700

    Time: 151 ms (72.73%), Space: 52.9 MB (78.79%) - LeetHub

--- 2302-count-subarrays-with-score-less-than-k/2302-count-subarrays-with-score-less-than-k.js
+++ 2302-count-subarrays-with-score-less-than-k/2302-count-subarrays-with-score-less-than-k.js
@@ -4,13 +4,13 @@
  * @return {number}
  */
 var countSubarrays = function(nums, k) {
-    let sum = 0
-    let count = 0
+    let sum = 0;
+    let count = 0;
     let j = 0;
     for(let i = 0; i < nums.length; i++){
-        sum += nums[i]
-        while(sum * (1+i-j) >=k) sum -= nums[j++]
-        count += (1+i-j)
+        sum += nums[i];
+        while(sum * (1+i-j) >=k) sum -= nums[j++];
+        count += (1+i-j);
     }
-    return count
+    return count;
 };
