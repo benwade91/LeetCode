4d59aa25d45409b6dce22bc21379723ec3ecdf13
Ben Wade <70988204+benwade91@users.noreply.github.com>
2022-10-28 11:42:43 -0700

    Time: 91 ms (76.42%), Space: 42.9 MB (47.49%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.js
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.js
@@ -3,10 +3,9 @@
  * @return {number}
  */
 var maxProduct = function(nums) {
-    let res = Math.max(...nums);
-    let min = 1;
-    let max = 1;
-    for(let i = 0; i < nums.length; i++){
+    let [res, min, max, len] = [Math.max(...nums), 1, 1, nums.length]
+    
+    for(let i = 0; i < len; i++){
         let curr = max;
         max = Math.max(curr * nums[i], min * nums[i], nums[i])
         min = Math.min(curr * nums[i], min * nums[i], nums[i])
