5b647b7065747eb6d330a9f45a4809639b6472fa
Ben Wade <70988204+benwade91@users.noreply.github.com>
2022-10-31 15:41:35 -0700

    Time: 109 ms (41.49%), Space: 44.3 MB (17.63%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.js
+++ 1046-last-stone-weight/1046-last-stone-weight.js
@@ -3,11 +3,24 @@
  * @return {number}
  */
 var lastStoneWeight = function(stones) {
+    stones.sort((a,b)=>a-b);
+    
     while(stones.length > 1){
-        stones.sort((a,b) => a-b);
         let a = stones.pop();
         let b = stones.pop();
         stones.push(Math.abs(a-b))
+        let i = stones.length-2;
+        let sorted = false;
+        while(!sorted){
+            if(stones[stones.length-1] > stones[i]) sorted = true;
+            else {
+                while(stones[i+1] < stones[i]){
+                    [stones[i+1], stones[i]] = [stones[i], stones[i+1]]
+                    i--;
+                }
+                sorted = true;
+            }
+        }
     }
     return stones[0] || 0;
 };
