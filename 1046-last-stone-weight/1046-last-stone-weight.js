2599d28935090e0dafbc8dc9bb024fdbf8205127
Ben Wade <70988204+benwade91@users.noreply.github.com>
2022-10-31 15:43:13 -0700

    Time: 71 ms (88.53%), Space: 44.1 MB (25.61%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.js
+++ 1046-last-stone-weight/1046-last-stone-weight.js
@@ -3,8 +3,8 @@
  * @return {number}
  */
 var lastStoneWeight = function(stones) {
+//     sort stones in ascending order
     stones.sort((a,b)=>a-b);
-    
     while(stones.length > 1){
         let a = stones.pop();
         let b = stones.pop();
@@ -12,8 +12,10 @@ var lastStoneWeight = function(stones) {
         let i = stones.length-2;
         let sorted = false;
         while(!sorted){
+//             if last stone is greatest, stones are sorted
             if(stones[stones.length-1] > stones[i]) sorted = true;
             else {
+//                 while stone is out of order, bubble down;
                 while(stones[i+1] < stones[i]){
                     [stones[i+1], stones[i]] = [stones[i], stones[i+1]]
                     i--;
