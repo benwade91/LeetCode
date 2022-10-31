0e5941beb428c0b2b060adb06a06da1863b36ef0
Ben Wade <70988204+benwade91@users.noreply.github.com>
2022-10-31 15:29:52 -0700

    Time: 77 ms (84.12%), Space: 43.5 MB (56.46%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} stones
+ * @return {number}
+ */
+var lastStoneWeight = function(stones) {
+    while(stones.length > 1){
+        stones.sort((a,b) => a-b);
+        let a = stones.pop();
+        let b = stones.pop();
+        stones.push(Math.abs(a-b))
+    }
+    return stones[0] || 0;
+};
