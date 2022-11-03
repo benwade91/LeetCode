a925602b7d861749e934dacd176f2fd335a1326e
Ben Wade <70988204+benwade91@users.noreply.github.com>
2022-11-03 10:59:33 -0700

    Time: 149 ms (50.04%), Space: 45.9 MB (85.69%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.js
+++ 0006-zigzag-conversion/0006-zigzag-conversion.js
@@ -5,26 +5,13 @@
  */
 var convert = function(s, numRows) {
     if(numRows == 1) return s
-    let table = Array(numRows).fill().map(()=>Array().fill())
-    let x = 0, y = 0, dir = 0;
+    let table = Array(numRows).fill("")
+    let dir = false;
+    let row = 0;
     for(let c of s){
-        table[x][y] = c;
-        if(dir){
-            if(x < table.length-1)x++
-            else{
-                x--;
-                y++;
-                dir = 0
-            }
-        }else{
-            if(x > 0){
-                x--
-                y++
-            }else{
-                x++
-                dir = 1
-            }
-        }
+        table[row] += c;
+        if(row == 0 || row == numRows-1) dir = !dir;
+        row += dir ? 1 : -1;
     }
-    return table.flat().join("")
+    return table.join("")
 };
