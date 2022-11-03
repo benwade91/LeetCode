a6994ddfab9f57b259b16e457fa612e6fbfdfd63
Ben Wade <70988204+benwade91@users.noreply.github.com>
2022-11-03 10:18:52 -0700

    Time: 120 ms (68.01%), Space: 49.9 MB (37.22%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.js
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -7,9 +7,9 @@ var maxArea = function(height) {
     let l = 0, r = height.length-1;
     
     while(l<r){
-        let w = r-l
-        let h = Math.min(height[r], height[l])
-        let currArea = w*h;
+        const w = r-l
+        const h = Math.min(height[r], height[l])
+        const currArea = w*h;
         area = Math.max(currArea, area);
         if(height[l] > height[r]) r--
         else l++
