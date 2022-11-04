48401e84fc65b720c75f64f01d8f5eda1bd911b9
Ben Wade <70988204+benwade91@users.noreply.github.com>
2022-11-04 11:26:14 -0700

    Time: 82 ms (72.55%), Space: 41.5 MB (92.66%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.js
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -3,7 +3,9 @@
  * @return {number}
  */
 var climbStairs = function(n) {
-    let dp = [1,2]
+    let dp = Array(n);
+    dp[0] = 1, dp[1] = 2;
+    
     for(let i = 2; i < n; i++){
         dp[i] = dp[i-1] + dp[i-2];
     }
