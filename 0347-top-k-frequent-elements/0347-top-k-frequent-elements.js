349f703643c9a2f9c5b88f04735689b47e8027a6
Ben Wade <70988204+benwade91@users.noreply.github.com>
2022-11-07 08:42:03 -0800

    Time: 140 ms (39.54%), Space: 46.2 MB (44.59%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
@@ -4,22 +4,12 @@
  * @return {number[]}
  */
 var topKFrequent = function(nums, k) {
-//     keep count of each element occurence in map
     const map = {};
-//     create results list
-    const res = [];
-//     iterate over nums and add count to map;
-    for(let num of nums) map[num] = (map[num] ?? 0) + 1;
-
-//     create currMaxEl value
-    let currMaxEl = nums[0];
-//     iterate over map for k largest values
-    while(res.length < k){
-        for(let key in map){
-            if(map[key] > map[currMaxEl]) currMaxEl = key
-        }
-        res.push(currMaxEl)
-        map[currMaxEl] = 0;
-    }
+    let res = [];
+    for(const num of nums) map[num] = map[num] + 1 || 1;
+    for(const key in map) res.push(key)
+    res.sort((a,b)=>map[b]-map[a])
+    res.length = k;
+    console.log(map)
     return res
 };
