74bb4d88c13db8d89817deb70d5e8bd9dba2b1cd
Ben Wade <70988204+benwade91@users.noreply.github.com>
2022-11-07 08:57:03 -0800

    Time: 111 ms (18.56%), Space: 41.8 MB (80.07%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
@@ -5,9 +5,9 @@
 var findMin = function(nums) {
     let [l,r] = [0, nums.length-1];
     while(l < r){
-        let mid = Math.floor((r+l) / 2);
-        if(nums[r] < nums[mid]) l = mid+1;
-        else r = mid;
+        const mid = Math.floor((l+r)/2);
+        if(nums[r] < nums[mid]) l = mid+1
+        else r = mid
     }
-    return nums[l];
+    return nums[l]
 };
