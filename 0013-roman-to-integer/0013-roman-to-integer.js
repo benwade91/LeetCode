26c6a9f7b0c96c9467ed361ad7f931917ab21449
Ben Wade <70988204+benwade91@users.noreply.github.com>
2022-11-30 18:13:28 +0000

    Time: 470 ms (5.00%), Space: 53.6 MB (5.12%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const map = {
+    "I": 1,
+    "V": 5,
+    "X": 10,
+    "L": 50,
+    "C": 100,
+    "D": 500,
+    "M": 1000
+}
+
+var romanToInt = function(s) {
+    let res = 0;
+    if(s.length == 1) return map[s[0]]
+    for(let i = 0; i < s.length; i++){
+        console.log(res)
+        let a = map[s[i]]
+        let b = map[s[i+1]]
+        if(a >= b || !b) res += a;
+        else{
+            res += b-a;
+            i++
+        }
+    }
+    return res
+};
